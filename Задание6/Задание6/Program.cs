bd78167a5f39621180f59a01d10580fe273a723f
OnIaG592 <huseynhaciev@gmail.com>
2021-04-29 14:33:36 +0400

    dopolnenie k6 zadaniyu

--- Задание6/Задание6/Program.cs
+++ Задание6/Задание6/Program.cs
@@ -6,65 +6,115 @@ namespace Задание6
     {
         static void Main(string[] args)
         {
-            int num1 = AskAge();
-            int num2 = AskAge();
-            int num3 = AskAge();
-            int num4 = AskAge();
-            Random a = new Random();
-            string value =Convert.ToString(a.Next(1000,9999));
-            Console.WriteLine(value);
-            var ask = Game(num1, num2, num3, num4);
-            
-              /*  for (int i = 0; i < 4; i++ )
-             while( value[i]!=value[i+1]);*/
-
 
             
-        }
 
-        static string Game (int num1,int num2,int num3,int num4,string x)
-        {
-            int korovi = 0;
-            int biki = 0;
-            int popitki = 0;
+            
 
-            if (korovi == 4)
+            static int [] Random()
             {
-                Console.WriteLine("Вы угадали число");
-            }
-            else if (num1 == x[0]) { korovi++; }
-            else if (num2 == x[1]) { korovi++; }
-            else if (num3 == x[2]) { korovi++; }
-            else if (num4 == x[3]) { korovi++; }
-            else if ()
+            bool proverka = true;
+                int values1 =0;
+                int values2 = 0;
+                int values3 = 0;
+                int values4 = 0;
 
-        }
-
-        static int AskAge()
-        {
-            Console.WriteLine("Введите 4-x значное число");
-            bool a = false;
-            int num = 0;
-            do
+                while (proverka == true)
             {
-                try
-                {
-                    num = Convert.ToInt32(Console.ReadLine());
-                    if (num >= 0)
-                    {
-                        a = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Не правильный ввод;\nВведите заного");
-                    }
+                Random a = new Random();
+                   values1 = a.Next(0, 9);
+                   values2 = a.Next(0, 9);
+                   values3 = a.Next(0, 9);
+                   values4 = a.Next(0, 9);
+
+
+                    if (values1 == values2 || values1 == values3 || values1 == values4 || values2 == values3 || values2 == values4 || values3 == values4) { proverka = true; }
+                else { proverka = false;  }
+
                 }
-                catch { Console.WriteLine("Не правильный ввод;\nВведите заного"); }
+                int[] value = new int[4] { values1, values2, values3, values4 };
+                return value;
             }
-            while (a == false);
-            return num;
-        }
-        
 
+            string result =Game(Random(),num:0);
+
+            Console.WriteLine(result);
+
+            static string Game (int[] value,int num)
+            {
+                
+                int biki = 0;
+                int num1 = AskAge();
+                int num2 = AskAge();
+                int num3 = AskAge();
+                int num4 = AskAge();
+
+                int[] first = new int[4] { num1, num2, num3, num4 };
+                int korovi = 0;
+                
+                string c = "Вы отгадали число";
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    for (int s = 0; s < first.Length; s++)
+                    {
+                        if (first[s] == value[i])
+                        {
+                            biki++;
+                            break;
+                        }
+
+                    }
+                    if (value[i] == first[i])
+                    {
+                        korovi++;
+                    }
+
+                }
+
+                if (korovi == 4) { return c; }
+                else
+                {
+                    Console.WriteLine($"korovi={korovi},biki={biki}");
+                    return Game(value,num++); }
+
+            }
+            Console.WriteLine();
+            
+
+           
+
+
+
+
+
+
+            static int AskAge()
+            {
+                Console.WriteLine("Введите число");
+                bool a = false;
+                int num = 0;
+                do
+                {
+                    try
+                    {
+                        num = Convert.ToInt32(Console.ReadLine());
+                        if (num >= 0)
+                        {
+                            a = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не правильный ввод;\nВведите заного");
+                        }
+                    }
+                    catch { Console.WriteLine("Не правильный ввод;\nВведите заного"); }
+                }
+                while (a == false);
+                return num;
+            }
+
+
+        }
     }
 }
